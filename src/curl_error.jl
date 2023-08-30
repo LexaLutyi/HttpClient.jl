51403e500e8e1ecad5db53e37a9a9e09d3e9f604
Samarin Aleksei <liotbiu1@gmail.com>
2023-08-30 12:44:24 +0400

    curl code to str

--- src/curl_error.jl
+++ src/curl_error.jl
@@ -1,8 +1,11 @@
+curl_code_to_string(code) = unsafe_string(curl_easy_strerror(code))
+
+
 macro curlok(exp)
     quote
         local rc = $(esc(exp))
         if rc != CURLE_OK
-            curl_error = unsafe_string(curl_easy_strerror(rc))
+            curl_error = curl_code_to_string(rc)
             error(curl_error)
         end
         rc
