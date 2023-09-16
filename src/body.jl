cf58fa75c79b88217bc581d20c22dff796ee2fb9
Samarin Aleksei <liotbiu1@gmail.com>
2023-09-16 16:27:12 +0400

    support bytes as body

--- src/body.jl
+++ src/body.jl
@@ -1,4 +1,14 @@
-function set_body(curl, body)
-    body = isnothing(body) ? Vector{UInt8}() : body
+function set_body(curl, body::Vector{UInt8})
+    @curlok curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body)
+    @curlok curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, length(body))
+end
+
+
+function set_body(curl, ::Nothing)
+    set_body(curl, "")
+end
+
+
+function set_body(curl, body::AbstractString)
     @curlok curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body)
 end
