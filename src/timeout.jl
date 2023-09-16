ec039f609c169a39fe51e6196afe06e2d064043b
Samarin Aleksei <liotbiu1@gmail.com>
2023-09-16 15:58:18 +0400

    support Float timeout

--- src/timeout.jl
+++ src/timeout.jl
@@ -1,3 +1,9 @@
-function set_timeout(curl, timeout)
+function set_timeout(curl, timeout::Integer)
     @curlok curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout)
+end
+
+
+function set_timeout(curl, timeout::AbstractFloat)
+    ms = round(Int, 1000 * timeout)
+    @curlok curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, ms)
 end
